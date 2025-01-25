1b425a6da171a9e94ea40051a8cac5e50070b319
Abdurakhmanov_Daniyel <daniel.kapchagay@gmail.com>
2025-01-26 01:19:42 +0500

    Dictionaries

--- lab2.py
+++ lab2.py
@@ -831,3 +831,350 @@ symmetric_difference_update() 	^= 	Inserts the symmetric differences from this s
 union()                       	| 	Return a set containing the union of sets
 update() 	                      |= 	Update the set with the union of this set and others
 """
+
+
+#Dictionaries.
+#A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+#Dictionaries are written with curly brackets, and have keys and values:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+#Dictionary items are ordered, changeable, and do not allow duplicates.
+#Dictionary items are presented in key:value pairs, and can be referred to by using the key name.
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+#Dictionaries cannot have two items with the same key:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict) 
+
+#lem():
+print(len(thisdict))
+
+#The values in dictionary items can be of any data type:
+thisdict =	{
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+} 
+#type - 'dict'
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict)) 
+#The dict() Constructor:
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
+
+#Accessing Items.
+#You can access the items of a dictionary by referring to its key name, inside square brackets:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+print(x)
+
+#There is also a method called get() that will give you the same result:
+x = thisdict.get("model")
+print(x)
+
+#Get Keys.
+#The keys() method will return a list of all the keys in the dictionary.
+x = thisdict.keys()
+print(x)
+
+#The list of the keys is a view of the dictionary, meaning that any changes done to the dictionary will be reflected in the keys list.
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.keys()
+print(x) #before the change
+car["color"] = "white"
+print(x) #after the change
+
+#Get Values.
+#The values() method will return a list of all the values in the dictionary.
+x = thisdict.values() 
+print(x)
+
+#The list of the values is a view of the dictionary, meaning that any changes done to the dictionary will be reflected in the values list.
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.values()
+print(x) #before the change
+car["year"] = 2020
+print(x) #after the change 
+
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.values()
+print(x) #before the change
+car["color"] = "red"
+print(x) #after the change 
+
+#Get Items.
+#The items() method will return each item in a dictionary, as tuples in a list.
+x = thisdict.items() 
+print(x)
+
+#The returned list is a view of the items of the dictionary, meaning that any changes done to the dictionary will be reflected in the items list.
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.items()
+print(x) #before the change
+car["year"] = 2020
+print(x) #after the change 
+
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.items()
+print(x) #before the change
+car["color"] = "red"
+print(x) #after the change 
+
+#Check if Key Exists.
+#To determine if a specified key is present in a dictionary use the in keyword:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary") 
+
+#Change Values.
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
+print(thisdict)
+
+#Update Dictionary
+#The update() method will update the dictionary with the items from the given argument.
+#The argument must be a dictionary, or an iterable object with key:value pairs.
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020}) 
+print(thisdict)
+
+#Adding Items.
+#Adding an item to the dictionary is done by using a new index key and assigning a value to it:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+
+#update():
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"color": "red"}) 
+
+#Removing Items.
+#The pop() method removes the item with the specified key name:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict) 
+
+#The popitem() method removes the last inserted item:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.popitem()
+print(thisdict) 
+
+#The del keyword removes the item with the specified key name:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict["model"]
+print(thisdict) 
+
+#The del keyword can also delete the dictionary completely:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict
+#print(thisdict) this will cause an error because "thisdict" no longer exists. 
+
+#The clear() method empties the dictionary:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.clear()
+print(thisdict) 
+
+#Loop Through a Dictionary.
+#When looping through a dictionary, the return value are the keys of the dictionary, but there are methods to return the values as well.
+#Print all key names in the dictionary, one by one:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(x) 
+
+#Print all values in the dictionary, one by one:
+for x in thisdict:
+  print(thisdict[x]) 
+
+#values() for retrieving values:
+for x in thisdict.values():
+  print(x) 
+
+#keys() for retrieving keys:
+for x in thisdict.keys():
+  print(x) 
+
+#Loop through both keys and values, by using the items() method:
+for x, y in thisdict.items():
+  print(x, y) 
+
+
+#Copy a Dictionary
+#You cannot copy a dictionary simply by typing dict2 = dict1, because: dict2 will only be a reference to dict1, and changes made in dict1 will automatically also be made in dict2.
+#There are ways to make a copy, one way is to use the built-in Dictionary method copy()
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+#Another way to make a copy is to use the built-in function dict().
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict) 
+
+
+#Nested Dictionaries.
+#A dictionary can contain dictionaries, this is called nested dictionaries.
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+} 
+print(myfamily)
+
+#Or, if you want to add three dictionaries into a new dictionary:
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+
+myfamily = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+} 
+print(myfamily)
+
+#Access Items in Nested Dictionaries:
+#To access items from a nested dictionary, you use the name of the dictionaries, starting with the outer dictionary:
+print(myfamily["child2"]["name"]) 
+
+#Loop Through Nested Dictionaries.
+#You can loop through a dictionary by using the items() method like this:
+for x, obj in myfamily.items():
+  print(x)
+
+  for y in obj:
+    print(y + ':', obj[y])
+
+#Dict methods:
+"""
+clear()	Removes all the elements from the dictionary
+copy()	Returns a copy of the dictionary
+fromkeys()	Returns a dictionary with the specified keys and value
+get()	Returns the value of the specified key
+items()	Returns a list containing a tuple for each key value pair
+keys()	Returns a list containing the dictionary's keys
+pop()	Removes the element with the specified key
+popitem()	Removes the last inserted key-value pair
+setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+update()	Updates the dictionary with the specified key-value pairs
+values()	Returns a list of all the values in the dictionary
+"""
