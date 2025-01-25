b39a67f4c79f279d07d66e15fde3479c4b00ba5b
Abdurakhmanov_Daniyel <daniel.kapchagay@gmail.com>
2025-01-26 02:39:42 +0500

    While loops

--- lab2.py
+++ lab2.py
@@ -1286,3 +1286,45 @@ a = 33
 b = 200
 if b > a:
   pass
+
+#Python loops:
+"""
+Python has two primitive loop commands:
+
+    while loops
+    for loops
+"""
+#While loop:
+i = 1
+while i < 6:
+  print(i, end=" ")
+  i += 1
+print()
+#The break Statement.
+#With the break statement we can stop the loop even if the while condition is true:
+i = 1
+while i < 6:
+  print(i, end=" ")
+  if i == 3:
+    break
+  i += 1 
+print()
+#The continue Statement.
+#With the continue statement we can stop the current iteration, and continue with the next:
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i, end=" ")
+print()
+
+#The else Statement.
+#With the else statement we can run a block of code once when the condition no longer is true:
+i = 1
+while i < 6:
+  print(i, end=" ")
+  i += 1
+else:
+  print()
+  print("i is no longer less than 6")
