e91000cdb977bb785f32c1b7bc7d4485106f9093
Abdurakhmanov_Daniyel <daniel.kapchagay@gmail.com>
2025-01-25 20:18:46 +0500

    Operators

--- lab2.py
+++ lab2.py
@@ -69,3 +69,118 @@ else:
 #Also built-in functions can return boolean value:
 x = 200
 print(isinstance(x, int))
+
+
+#Operators:
+print(10 + 5)
+
+"""
+Python divides the operators in the following groups:
+
+  — Arithmetic operators
+  — Assignment operators
+  — Comparison operators
+  — Logical operators
+  — Identity operators
+  — Membership operators
+  — Bitwise operators
+"""
+"""
++ 	Addition	
+- 	Subtraction	
+* 	Multiplication	
+/ 	Division 	
+% 	Modulus	
+** 	Exponentiation	
+// 	Floor division
+"""
+"""
+Assignment operators are used to assign values to variables:
+
+= 	x = 5 	
++= 	x += 3 	
+-= 	x -= 3  	
+*= 	x *= 3 	
+/= 	x /= 3 	
+%= 	x %= 3	
+//= 	x //= 3	
+**= 	x **= 3	
+&= 	x &= 3	
+|= 	x |= 3	
+^= 	x ^= 3 	
+>>= 	x >>= 3
+<<= 	x <<= 3
+:= 	print(x := 3)
+"""
+
+"""
+Comparison operators are used to compare two values:
+
+== 	Equal 	
+!= 	Not equal
+> 	Greater than	
+< 	Less than
+>= 	Greater than or equal to	
+<= 	Less than or equal to
+"""
+
+"""
+Logical operators are used to combine conditional statements:
+
+and — Returns True if both statements are true
+or — Returns True if one of the statements is true
+not — Reverse the result, returns False if the result is true
+"""
+
+"""
+Identity operators are used to compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
+
+is — Returns True if both variables are the same object
+is not — Returns True if both variables are not the same object
+"""
+
+"""
+Membership operators are used to test if a sequence is presented in an object:
+
+in — Returns True if a sequence with the specified value is present in the object	
+not in — Returns True if a sequence with the specified value is not present in the object
+"""
+
+"""
+
+&  	AND 	Sets each bit to 1 if both bits are 1	
+| 	OR 	Sets each bit to 1 if one of two bits is 1
+^ 	XOR 	Sets each bit to 1 if only one of two bits is 1
+~ 	NOT 	Inverts all the bits
+<< 	Zero fill left shift 	Shift left by pushing zeros in from the right and let the leftmost bits fall off
+>> 	Signed right shift 	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+"""
+
+#Operator Precedence:
+
+#Parentheses has the highest precedence, meaning that expressions inside parentheses must be evaluated first:
+print((6 + 3) - (6 + 3)) 
+
+#Multiplication * has higher precedence than addition +, and therefor multiplications are evaluated before additions:
+print(100 + 5 * 3)
+
+"""
+Precedence from top to bottom:
+
+()  —  Parentheses 	
+**  —  Exponentiation 	
++x  -x  ~x  —  Unary plus, unary minus, and bitwise NOT 	
+*  /  //  %  —  Multiplication, division, floor division, and modulus 	
++  -  —  Addition and subtraction 	
+<<  >>  —  Bitwise left and right shifts 	
+&  —  Bitwise AND 	
+^  —  Bitwise XOR 	
+|  —  Bitwise OR 	
+==  !=  >  >=  <  <=  is  is not  in  not in  —  Comparisons, identity, and membership operators 	
+not  —  Logical NOT 	
+and  —  AND 	
+or  —  OR
+"""
+
+#If two or more operators have the same precedence, the expression is evaluated from left to right.
+print(5 + 4 - 7 + 3)
